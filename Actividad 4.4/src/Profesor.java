2d6b378508c4bbe6dff50293093c0f2a1d4af0b0
Alberto Luque <luquealberto230916@gmail.com>
2023-03-08 20:32:27 +0100

    Añadido de fichero, encapsulamiento de atributos, Métodos de una clase para acceder a un atribulo en lectura y escritura y extracción de un método para recorrer prestamos en la clase Profesor.

--- Actividad 4.4/src/Profesor.java
+++ Actividad 4.4/src/Profesor.java
@@ -1,12 +1,36 @@
 import java.util.List;
 
 public class Profesor extends Persona{
-    String str;
-    int edad;
-    List<Prestamo> prestamos;
+    private String str;
+    private int edad;
+    private List<Prestamo> prestamos;
 
     public Profesor(String numeroDeTelefono, String numeroDeTelefono1) {
         super(numeroDeTelefono);
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public List<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(List<Prestamo> prestamos) {
+        this.prestamos = prestamos;
     }
 
     public void printInformacionPersonal(){
@@ -19,6 +43,10 @@ public class Profesor extends Persona{
         System.out.println("Nombre: " + str);
         System.out.println("Edad: " + edad);
         System.out.println("Teléfono: " + numeroDeTelefono);
+        recorrerPrestamos();
+    }
+
+    private void recorrerPrestamos() {
         for (Prestamo p: prestamos) {
             System.out.println(p);
         }
